b670ee19e5b9ed36ccc58b3e5f8abaf26313bcd9
Siranjeevan <smartjeevith2k@gmail.com>
2025-08-24 02:12:37 +0530

    logo update

--- src/components/Header/Logo.jsx
+++ src/components/Header/Logo.jsx
@@ -1,11 +1,12 @@
 // Logo.jsx
+import Logo from "../../assets/Logo.png"
 export default function Logo() {
   return (
     <a href="#" className="-m-1.5 p-1.5">
       <span className="sr-only text-black">Your Company</span>
       <img
         className="h-13 ml-5 sm:h-15 md:h-16 xl:h-20 2xl:h-35 mx-auto rounded-full"
-        src="Logo.png"
+        src={Logo}
         alt="logo"
       />
     </a>
